683c1024e9071245f523295e5811eb7b1b441b84
Fabian Beyerlein <dev@beyerleinf.de>
2021-02-20 23:38:43 +0100

    chore(ci): Add build script

--- apps/rki-covid-dashboard/src/app/app-routing.module.ts
+++ apps/rki-covid-dashboard/src/app/app-routing.module.ts
@@ -5,24 +5,19 @@ const routes: Route[] = [
   { path: '', pathMatch: 'full', redirectTo: '/germany' },
   {
     path: 'germany',
-    loadChildren: () =>
-      import(/* webpackModuleName: 'germany' */ './germany/germany.module').then(m => m.GermanyModule),
+    loadChildren: () => import('./germany/germany.module').then(m => m.GermanyModule),
   },
   {
     path: 'states',
-    loadChildren: () => import(/* webpackModuleName: 'states' */ './states/states.module').then(m => m.StatesModule),
+    loadChildren: () => import('./states/states.module').then(m => m.StatesModule),
   },
   {
     path: 'districts',
-    loadChildren: () =>
-      import(/* webpackModuleName: 'districts' */ './districts/districts.module').then(m => m.DistrictsModule),
+    loadChildren: () => import('./districts/districts.module').then(m => m.DistrictsModule),
   },
   {
     path: 'vaccinations',
-    loadChildren: () =>
-      import(/* webpackModuleName: 'vaccinations' */ './vaccinations/vaccinations.module').then(
-        m => m.VaccinationsModule
-      ),
+    loadChildren: () => import('./vaccinations/vaccinations.module').then(m => m.VaccinationsModule),
   },
   { path: '**', redirectTo: '/germany' },
 ];
